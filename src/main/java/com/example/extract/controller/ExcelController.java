488f24694b5ea9d34469cfcc7a467e37e4c850ed
gravingu@163.com <gravingu@163.com>
2020-11-10 14:49:50 +0800

    1

---
+++ src/main/java/com/example/extract/controller/ExcelController.java
@@ -0,0 +1,11 @@
+package com.example.extract.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ExcelController {
+
+
+}
