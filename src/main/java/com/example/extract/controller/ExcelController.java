206caef3d2975faa19bda19b9e9f6bc6e7db1a3b
gravingu@163.com <gravingu@163.com>
2020-11-19 19:05:32 +0800

    1

--- src/main/java/com/example/extract/controller/ExcelController.java
+++ src/main/java/com/example/extract/controller/ExcelController.java
@@ -1,13 +1,39 @@
 package com.example.extract.controller;
 
+import com.example.extract.utils.ExcelUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @RestController
 @RequestMapping("/")
 public class ExcelController {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
-    @RequestMapping("/extract")
-    public void 
-
+    /**
+     * 导出
+     *
+     * @param response
+     */
+    @RequestMapping(value = "/export", method = RequestMethod.GET)
+    public void exportExcel(HttpServletResponse response) {
+        jdbcTemplate.query("select * from all_tables", new RowCallbackHandler() {
+            @Override
+            public void processRow(ResultSet resultSet) throws SQLException {
+                int columnCount = resultSet.getMetaData().getColumnCount();
+                for (int i = 0; i < columnCount; i++) {
+                    resultSet.getObject(i);
+                }
+            }
+        });
+        ExcelUtils.exportExcel(personList, "员工信息表", "员工信息", MonitorPersonExportVo.class, "员工信息", response);
+    }
 }
